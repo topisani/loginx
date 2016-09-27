ec9b3d8b40e8f59db729aaab09c85343e472dbe9
topisani <tobiaspisani@gmail.com>
2016-09-27 20:58:04 +0200

    Removed X server behaviour
    This can be done easily with a script in `.profile`, and was for some
    reason failing. Now works on an up to date arch installation with no changes!

--- usess.c
+++ usess.c
@@ -9,18 +9,18 @@
 
 static void QuitSignal (int sig);
 static void AlarmSignal (int sig);
-static void XreadySignal (int sig);
+/* static void XreadySignal (int sig); */
 static void ChildSignal (int sig);
 static void BecomeUser (const struct account* acct);
 static void RedirectToLog (void);
 static void WriteMotd (const struct account* acct);
-static pid_t LaunchX (const struct account* acct);
+/* static pid_t LaunchX (const struct account* acct); */
 static pid_t LaunchShell (const struct account* acct, const char* arg);
 
 //----------------------------------------------------------------------
 
 static bool _quitting = false;
-static bool _xready = false;
+/* static bool _xready = false; */
 static int _killsig = SIGTERM;
 
 //----------------------------------------------------------------------
@@ -28,13 +28,13 @@ static int _killsig = SIGTERM;
 void RunSession (const struct account* acct)
 {
     // Check if need to launch X
-    char xinitrcPath [PATH_MAX];
-    snprintf (xinitrcPath, sizeof(xinitrcPath), "%s/.xinitrc", acct->dir);
+    /* char xinitrcPath [PATH_MAX]; */
+    /* snprintf (xinitrcPath, sizeof(xinitrcPath), "%s/.xinitrc", acct->dir); */
 
-    pid_t xpid = 0;
-    if (0 == access (xinitrcPath, R_OK))
-	xpid = LaunchX (acct);
-    pid_t shellpid = LaunchShell (acct, xpid ? ".xinitrc" : NULL);
+    /* pid_t xpid = 0; */
+    /* if (0 == access (xinitrcPath, R_OK)) */
+	/* xpid = LaunchX (acct); */
+    pid_t shellpid = LaunchShell (acct, /* xpid ? ".xinitrc" : */ NULL);
     if (!shellpid)
 	return;
 
@@ -51,23 +51,23 @@ void RunSession (const struct account* acct)
     sigset_t smask;
     sigprocmask (SIG_UNBLOCK, NULL, &smask);
 
-    while (shellpid || xpid) {
+    while (shellpid /* || xpid */) {
 	sigsuspend (&smask);
 	int chldstat = 0;
 	pid_t cpid = waitpid (-1, &chldstat, WNOHANG);
-	if (cpid == shellpid || cpid == xpid) {
+	if (cpid == shellpid /* || cpid == xpid */) {
 	    if (cpid == shellpid)
 		shellpid = 0;
-	    else if (cpid == xpid)
-		xpid = 0;
+	    /* else if (cpid == xpid) */
+		/* xpid = 0; */
 	    _quitting = true;
 	    alarm (KILL_TIMEOUT);
 	}
 	if (_quitting) {
 	    if (shellpid)
 		kill (shellpid, _killsig);
-	    if (xpid)
-		kill (xpid, _killsig);
+	    /* if (xpid) */
+		/* kill (xpid, _killsig); */
 	}
     }
 
@@ -98,10 +98,10 @@ static void AlarmSignal (int sig __attribute__((unused)))
     _killsig = SIGKILL;
 }
 
-static void XreadySignal (int sig __attribute__((unused)))
-{
-    _xready = true;
-}
+/* static void XreadySignal (int sig __attribute__((unused))) */
+/* { */
+/*     _xready = true; */
+/* } */
 
 static void ChildSignal (int sig __attribute__((unused)))
 {
@@ -153,47 +153,47 @@ static void WriteMotd (const struct account* acct)
     fflush (stdout);
 }
 
-static pid_t LaunchX (const struct account* acct)
-{
-    signal (SIGUSR1, XreadySignal);
+/* static pid_t LaunchX (const struct account* acct) */
+/* { */
+/*     signal (SIGUSR1, XreadySignal); */
 
-    // Block delivery of SIGUSR1 until ready to avoid race conditions
-    sigset_t busr, orig;
-    sigemptyset (&busr);
-    sigaddset (&busr, SIGUSR1);
-    sigprocmask (SIG_BLOCK, &busr, &orig);
+/*     // Block delivery of SIGUSR1 until ready to avoid race conditions */
+/*     sigset_t busr, orig; */
+/*     sigemptyset (&busr); */
+/*     sigaddset (&busr, SIGUSR1); */
+/*     sigprocmask (SIG_BLOCK, &busr, &orig); */
 
-    pid_t pid = fork();
-    if (pid > 0) {
-	sigprocmask (SIG_SETMASK, &orig, NULL);	// Now unblock SIGUSR1
-	for (;;) {
-	    sigsuspend (&orig);	// Wait for SIGUSR1 from X before returning
-	    int ecode, rc = waitpid (pid, &ecode, WNOHANG);
-	    if (rc || errno != EINTR)
-		return (0);	// X failed to start, fallback to plain shell
-	    else if (_xready)
-		break;
-	}
-	return (pid);
-    } else if (pid < 0)
-	ExitWithError ("fork");
-    BecomeUser (acct);
-    unlink (PATH_SESSION_LOG);
-    RedirectToLog();
+/*     pid_t pid = fork(); */
+/*     if (pid > 0) { */
+/* 	sigprocmask (SIG_SETMASK, &orig, NULL);	// Now unblock SIGUSR1 */
+/* 	for (;;) { */
+/* 	    sigsuspend (&orig);	// Wait for SIGUSR1 from X before returning */
+/* 	    int ecode, rc = waitpid (pid, &ecode, WNOHANG); */
+/* 	    if (rc || errno != EINTR) */
+/* 		return (0);	// X failed to start, fallback to plain shell */
+/* 	    else if (_xready) */
+/* 		break; */
+/* 	} */
+/* 	return (pid); */
+/*     } else if (pid < 0) */
+/* 	ExitWithError ("fork"); */
+/*     BecomeUser (acct); */
+/*     unlink (PATH_SESSION_LOG); */
+/*     RedirectToLog(); */
 
-    signal (SIGTTIN, SIG_IGN);	// Ignore server reads and writes
-    signal (SIGTTOU, SIG_IGN);
-    signal (SIGUSR1, SIG_IGN);	// This tells the X server to send SIGUSR1 to parent when ready
-    sigprocmask (SIG_SETMASK, &orig, NULL);	// Now unblock SIGUSR1
+/*     signal (SIGTTIN, SIG_IGN);	// Ignore server reads and writes */
+/*     signal (SIGTTOU, SIG_IGN); */
+/*     signal (SIGUSR1, SIG_IGN);	// This tells the X server to send SIGUSR1 to parent when ready */
+/*     sigprocmask (SIG_SETMASK, &orig, NULL);	// Now unblock SIGUSR1 */
 
-    char vtname[] = "vt01";
-    vtname[3] = _ttypath[strlen(_ttypath)-1];
-    const char* argv[] = { "X", ":0", vtname, "-quiet", "-nolisten", "tcp", "-auth", ".config/Xauthority", NULL };
-    if (0 != access (argv[7], R_OK))
-	argv[6] = NULL;
-    execvp ("/usr/bin/X", (char* const*) argv);
-    ExitWithError ("execvp");
-}
+/*     char vtname[] = "vt01"; */
+/*     vtname[3] = _ttypath[strlen(_ttypath)-1]; */
+/*     const char* argv[] = { "X", ":0", vtname, "-quiet", "-nolisten", "tcp", "-auth", ".config/Xauthority", NULL }; */
+/*     if (0 != access (argv[7], R_OK)) */
+/* 	argv[6] = NULL; */
+/*     execvp ("/usr/bin/X", (char* const*) argv); */
+/*     ExitWithError ("execvp"); */
+/* } */
 
 static pid_t LaunchShell (const struct account* acct, const char* arg)
 {
